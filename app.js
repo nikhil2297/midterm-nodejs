8c56ce7bfa91cb28a791c4553bed1a4441cb8838
Shreyas Singh <singhshreyas906@gmail.com>
2024-10-18 16:08:01 -0400

    chaning port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Serve the static files in the public directory
 app.use(express.static(path.join(__dirname, 'public')));
