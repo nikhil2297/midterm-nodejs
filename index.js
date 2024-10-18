6eda729789b361d719e817f35e43ac9dd7e81f04
Shreyas Singh <singhshreyas906@gmail.com>
2024-10-18 15:43:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve the static files in the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle root route and return index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
